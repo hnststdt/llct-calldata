9b103661a288511ebbe3737964a17a62b62f44b3
hnststdt <hnststdt@gmail.com>
2023-01-14 11:14:20 +0900

    Use Pages as static api for llct

--- utils/makedist.py
+++ utils/makedist.py
@@ -1,14 +1,27 @@
+#!/usr/bin/python
 import os
 import json
 import shutil
 
+def as_response(data):
+    return {
+        'result': 'success',
+        'data': data
+    }
+
 if __name__ == '__main__':
     os.mkdir('dist')
+
+    shutil.copy('index.html', 'dist')
 
     with open('lists.json') as f:
         metadata = json.load(f)
 
-    shutil.copy('index.html', 'dist')
-    shutil.copy('lists.json', 'dist')
-    for group in metadata['groups']:
-        shutil.copytree(group['id'], f"dist/{group['id']}")
+    with open('dist/lists', 'w') as f:
+        json.dump(as_response(metadata), f)
+
+    os.mkdir('dist/call')
+    for gid, group in enumerate(metadata['groups']):
+        for sid in range(len(metadata['songs'][gid])):
+            if (os.path.exists(f"{group['id']}/{sid+1}")):
+                shutil.copy(f"{group['id']}/{sid+1}/call.json" ,f"dist/call/{gid}{sid+1}")
